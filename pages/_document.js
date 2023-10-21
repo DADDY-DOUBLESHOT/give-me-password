32c90ee1e70fb6a77f35b8c09be4dfe8bfc550a5
pratham-FC <prathams1001@gmail.com>
2023-10-22 02:20:14 +0530

    ui for passcode generator

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,13 @@ export default function Document() {
           integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
           crossOrigin="anonymous"
         />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
       </Head>
       <body>
         <Main />
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,32 @@ import { useState } from "react";
 
 export default function Home() {
   const [loader, setLoader] = useState(false);
+  const [show, setShow] = useState(false);
   const [code, setCode] = useState("");
 
   const handleSubmit = async () => {
+    setLoader(true);
     let temp = code;
     await setCode("");
     await generateCode(temp);
+  };
+
+  const copytoClipboard = () => {
+    const copy_btn = document.getElementById("copy-icon");
+    copy_btn.classList.remove("fa-regular");
+    copy_btn.classList.add("fa-solid");
+    navigator.clipboard.writeText(code);
+    setTimeout(() => {
+      copy_btn.classList.remove("fa-solid");
+      copy_btn.classList.add("fa-regular");
+    }, 2000);
+  };
+
+  const handleCodeShow = () => {
+    setTimeout(() => {
+      setShow(false);
+      setCode("");
+    }, 15000);
   };
 
   const generateCode = async () => {
@@ -21,6 +41,13 @@ export default function Home() {
       })
       .catch((err) => {
         console.log("got error", err);
+      })
+      .finally(() => {
+        setTimeout(() => {
+          setLoader(false);
+          setShow(true);
+          handleCodeShow();
+        }, 1000);
       });
   };
 
@@ -33,20 +60,55 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="container ">
-        <div style={{ minHeight: "100vh" }} className="d-flex justify-content-center align-items-center">
-          <div className="col-6 form-group">
-            <input
-              value={code}
-              onChange={(e) => {
-                setCode(e.target.value);
-              }}
-              className="form-control form-control"
-              placeholder="Any favourite word ?"
-            />
-            <button className="form-control btn btn-primary mt-3" onClick={() => handleSubmit()}>
-              Generate
-            </button>
-          </div>
+        <div style={{ minHeight: "100vh" }} className="d-flex justify-content-center">
+          {show ? (
+            <div className="col-12 col-lg-6 form-group d-flex flex-column">
+              <div className="d-flex flex-column justify-content-center align-items-center  flex-fill text-center">
+                <h5 className="mb-5">Please remember this code as this will be used for generation of other password</h5>
+
+                <div className="form-group d-inline-flex align-items-center">
+                  <input value={code} disabled className="form-control" id="code" />
+                  <i
+                    id="copy-icon"
+                    htmlFor="code"
+                    className="fa-regular fa-copy text-primary btn"
+                    onClick={() => {
+                      copytoClipboard();
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="col-12 col-lg-6 form-group d-flex flex-column">
+              <div className="d-flex flex-column justify-content-center align-items-center  flex-fill text-center">
+                <h5>Enter any favourite keyword and click generate</h5>
+                <img src="/images/or.svg" />
+
+                <h5>Directly click on generate</h5>
+              </div>
+
+              <div className="d-flex flex-column align-items-center flex-fill">
+                {loader ? (
+                  <span class="loader"></span>
+                ) : (
+                  <>
+                    <input
+                      value={code}
+                      onChange={(e) => {
+                        setCode(e.target.value);
+                      }}
+                      className="form-control form-control"
+                      placeholder="Any favourite word ?"
+                    />
+                    <button className="form-control btn btn-primary mt-3" onClick={() => handleSubmit()}>
+                      Generate
+                    </button>
+                  </>
+                )}
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
