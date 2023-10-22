30bfa4ed0000d95f00b8b1d5a60d0b47c8126593
pratham-FC <prathams1001@gmail.com>
2023-10-22 16:01:53 +0530

    url update

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-let tempserver = process.env.VERCEL_URL;
+let tempserver = `http://${process.env.VERCEL_URL}/api`;
 
 if (process && process.env.NODE_ENV === "development") {
   tempserver = "http://localhost:3000/api";
