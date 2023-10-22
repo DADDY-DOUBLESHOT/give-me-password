ca122e3a8de9df5d5453fd11662e21bf935f2626
pratham-FC <prathams1001@gmail.com>
2023-10-22 16:04:05 +0530

    url update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "production" ? `/${process.env.VERCEL_URL}` : "",
+  // basePath: process.env.NODE_ENV === "production" ? `/${process.env.VERCEL_URL}` : "",
 };
 
 module.exports = nextConfig;
